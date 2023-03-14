2bec5556d42f82cb68bcd17786bfe53bb99e3bf9
John L. Whiteman <>
2023-03-14 19:52:38 -0400

    More printf vuln
    Signed-off-by: John L. Whiteman <>

--- exercises/secure-coding/printfv.c
+++ exercises/secure-coding/printfv.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-void sayHi(const char *name) {
-	printf("Hi ");
+void sayHello(const char *name) {
+    /* Should be printf("Hello, %s", name); */
+	printf("Hello, ");
 	printf(name);
 	printf("\n");
 }
@@ -10,6 +11,6 @@ void sayHi(const char *name) {
 int main(int argc, char **argv) {
 	char name[64];
 	strncpy(name, argv[1], sizeof(name));
-	sayHi(name);
+	sayHello(name);
 	return 0;
 }
